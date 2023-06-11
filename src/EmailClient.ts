c8b898a8bc76fec333d6836106c87fbac2238143
Henrik Hofmeister <henrik@newdawn.dk>
2023-06-11 12:48:15 +0200

    Use nodemailer and add react rendering support

--- src/EmailClient.ts
+++ src/EmailClient.ts
@@ -1,9 +1,7 @@
+import React from "react";
 import Config, { ConfigProvider, ResourceInfo } from '@kapeta/sdk-config';
-import { SMTPClient, MessageHeaders,AUTH_METHODS }  from 'emailjs';
-const RESOURCE_TYPE = 'kapeta/resource-type-smtp-client';
-const PORT_TYPE = 'smtp';
-type SimpleSendOpts = Omit<MessageHeaders, "from"> & {from?: string};
-export type EmailSendOptions = MessageHeaders|SimpleSendOpts;
+import NodeMailer, {SendMailOptions, SentMessageInfo, Transporter} from 'nodemailer';
+import {render} from "@react-email/render";
 
 function trueIsh(value: any) {
     if (!value) {
@@ -17,11 +15,16 @@ function trueIsh(value: any) {
     return value === '1' || value === 'true' || value === 'yes';
 }
 
-export * from 'emailjs';
+export const RESOURCE_TYPE = 'kapeta/resource-type-smtp-client';
+export const PORT_TYPE = 'smtp';
+
+export type SendReactMailOptions = Omit<SendMailOptions,'text'|'html'>  & { body:React.ReactElement }
+
+export * from 'nodemailer';
 export class EmailClient {
     private _ready: boolean = false;
     private _smtpInfo?: ResourceInfo;
-    private _client?: SMTPClient;
+    private _transport?: Transporter<SentMessageInfo>;
 
     /**
      * Initialise SMTP client
@@ -43,25 +46,21 @@ export class EmailClient {
      */
     async init(provider: ConfigProvider) {
         this._smtpInfo = await provider.getResourceInfo(RESOURCE_TYPE, PORT_TYPE, PORT_TYPE);
-        const authentication = this._smtpInfo.options?.authentication
-            ? (this._smtpInfo.options?.authentication.split(',') as (keyof typeof AUTH_METHODS)[])
-            : [AUTH_METHODS.PLAIN];
 
-        console.log('Connecting to SMTP server at %s:%s', this._smtpInfo.host, this._smtpInfo.port);
-
-        const opts = {
+        this._transport = NodeMailer.createTransport({
             host: this._smtpInfo.host,
             port: parseInt('' + this._smtpInfo.port),
-            domain: this._smtpInfo.host,
-            user: this._smtpInfo.credentials?.username,
-            password: this._smtpInfo.credentials?.password,
-            ssl: trueIsh(this._smtpInfo.options?.ssl),
-            tls: trueIsh(this._smtpInfo.options?.tls),
-            authentication: this._smtpInfo.credentials?.username ? authentication : undefined,
-        }
-        this._client = new SMTPClient(opts);
+            secure: trueIsh(this._smtpInfo.options?.tls),
+            auth: {
+                user: this._smtpInfo.credentials?.username,
+                pass: this._smtpInfo.credentials?.password,
+            }
+        });
+        console.log('Connecting to SMTP server at %s:%s', this._smtpInfo.host, this._smtpInfo.port);
+
+
         try {
-            await this._testConnection();
+            await this._transport.verify();
         } catch (err) {
             console.error('Failed to connect to SMTP server at %s:%s', this._smtpInfo.host, this._smtpInfo.port, err);
             throw new Error('Failed to connect to SMTP server');
@@ -72,39 +71,43 @@ export class EmailClient {
         this._ready = true;
     }
 
-    async send(message: MessageHeaders|SimpleSendOpts) {
+    public async send(opts: SendMailOptions): Promise<SentMessageInfo> {
         if (!this._ready) {
             throw new Error('SMTPClient not ready');
         }
 
-        if (!message.from) {
+        if (!opts.from) {
             throw new Error('Missing from address');
         }
 
-        return this._client!.sendAsync(message as MessageHeaders);
-    }
-
-    /**
-     *
-     * @return {EmailJS.SMTPConnection}
-     */
-    get smtp() {
-        if (!this._ready) {
-            throw new Error('SMTPClient not ready');
+        if (!opts.subject) {
+            throw new Error('Missing subject');
         }
 
-        return this._client!.smtp;
+        return this.transport.sendMail(opts);
     }
 
-    async _testConnection() {
-        return new Promise((resolve, reject) => {
-            this._client!.smtp.connect((err) => {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-                resolve(null);
-            });
+    public async sendReact(opts: SendReactMailOptions): Promise<SentMessageInfo> {
+        const html = render(opts.body, {
+            plainText: false
         });
+
+        const text = render(opts.body, {
+            plainText: true
+        });
+
+        return this.send({
+            ...opts,
+            html,
+            text
+        });
+    }
+
+    public get transport():Transporter<SentMessageInfo> {
+        if (!this._ready) {
+            throw new Error('SMTP Transport not ready');
+        }
+
+        return this._transport!;
     }
 }
